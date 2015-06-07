071ad26bb635f9574a791b2625db4f1fcdbf7b41
simacna <simacna@gmail.com>
2015-06-07 18:20:17 -0400

    ammending the jquery src file

--- JavaScript/Geolocation/geolocation.js
+++ JavaScript/Geolocation/geolocation.js
@@ -6,7 +6,31 @@
 //navigator object has two objects:
 //1. getCurrentPosition
 
-function callback(position){
-	
+// function callback(position){
+
+// }
+// navigator.geolocation.getCurrentPosition(callback);
+
+$("#error").hide();
+$("#hud").show();
+
+navigator.geolocation.getCurrentPosition(gotLocation);
+
+function gotLocation(currentPosition) {
+  $("#hud").hide();
+
+  var $restaurants = $("span");
+  
+  $restaurants.each(function(){
+    var restaurantLatitude = $(this).data("lat");
+    var restaurantLongitude = $(this).data("lon");
+    
+    var distanceInMiles = calculateDistance(currentPosition.coords.latitude, currentPosition.coords.longitude, restaurantLatitude, restaurantLongitude);
+    
+    $(this).text(distanceInMiles + " miles");
+  });
 }
-navigator.geolocation.getCurrentPosition(callback);
+
+function displayError(message) {
+  $("#error").text(message).slideDown("slow");
+}
