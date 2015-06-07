914969a787573d889d7ef5d0af5d07975741ec17
simacna <simacna@gmail.com>
2015-06-07 18:10:13 -0400

    new geolocation practice

---
+++ JavaScript/Geolocation/geolocation.js
@@ -0,0 +1,12 @@
+//geolocation
+//automatic and accurate, user friendly,
+//used on both mobile (gps) and desktop (wifi)
+
+
+//navigator object has two objects:
+//1. getCurrentPosition
+
+function callback(position){
+	
+}
+navigator.geolocation.getCurrentPosition(callback);
