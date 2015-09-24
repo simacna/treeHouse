e87aa6ab6bce4aef37260376334ca3c6d81b03c5
sina sima <simacna@gmail.com>
2015-09-24 13:53:40 -0400

    more fiddling with test file

--- Python/test.py
+++ Python/test.py
@@ -14,32 +14,54 @@ dicts = [
 ]
 
 string = "Hi, I'm {name} and I love to eat {food}!"
+#
+# def string_factory(d, s):
+#   string_list = []
+#
+#   for e in d:
+#     string_list.append(s.format(**e))
+#
+#   return string_list
+#
+# # for k
+# # print(test)
+#
+# d = {'x': 1, 'y': 2, 'z': 3}
+#
+# # for key in d:
+# #     print(key, 'corresponds to', d[key])
+#
+# # for idx in dicts:
+# #     print(dicts[idx]['name'])
+#
+# class Store:
+#   open = 9
+#   close = 5
+#
+#   def hours(self, open,close):
+#     return "We're open from {open} to {close}".format(open, close)
+#
+# newStore = Store []
+# print(newStore.hours(9,5))
 
-def string_factory(d, s):
-  string_list = []
+import math
 
-  for e in d:
-    string_list.append(s.format(**e))
+def square(input):
+    return [i**2 for i in input]
 
-  return string_list
 
-# for k
-# print(test)
 
-d = {'x': 1, 'y': 2, 'z': 3}
+    # spaceholder = []
+    # for idx in input:
+    #     a = idx*idx
+    #     spaceholder.append(a)
+    #     # a = idx*idx
+    #     # spaceholder.append(a)
+    #
+    # return spaceholder
 
-# for key in d:
-#     print(key, 'corresponds to', d[key])
 
-# for idx in dicts:
-#     print(dicts[idx]['name'])
 
-class Store:
-  open = 9
-  close = 5
 
-  def hours(self, open,close):
-    return "We're open from {open} to {close}".format(open, close)
 
-newStore = Store
-print(newStore.hours(9,5))
+print(square([6,8,4]))
